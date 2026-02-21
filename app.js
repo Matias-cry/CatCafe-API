87f84ac947bf120d4689ff681a7602c3bce2b70c
Matias <matibravopro@gmail.com>
2026-02-21 01:26:41 +0000

    Fix rutas 3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use("/api/users", require("/routes/user.routes"));
+app.use("/api/users", require("/routes/userRoutes"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
